976ec6f62f0c581d74a6a88660fb883f074c64dc
Matt Rigby <matt.rigby@bristol.ac.uk>
2025-07-24 13:50:01 -0400

    Added script and Dockerfile to create container that can be run on GCP for downloading ERA 5

---
+++ ml_baselines/gcp_era5/README.md
@@ -0,0 +1,37 @@
+# Instructions for building and running Docker image on Google Cloud Platform
+
+### Setup
+1. ```gcloud``` is installed locally
+2. Docker desktop is installed locally
+3. ```buildx``` is installed to allow build for different architectures:
+
+```
+docker buildx create --use
+```
+
+4. Authentication:
+   1. ```gcloud auth configure-docker```
+5. Enable Cloud Run on GCP
+6. Set up a bucket to store the output on GCP
+7. Enable Artifact Repository on GCP
+
+### Building and deploying the image
+
+- From within the gcp_era5 folder, build the image for amd64 (or other relevant architecture) and push to Artifact Registry:
+
+```
+docker buildx build --platform linux/amd64 -t us-central1-docker.pkg.dev/my-project/my-repo/gcp-era5:latest --push .
+```
+(replace ```us-central1```, ```my-project``` and ```my-repo``` with desired server, project and Artifact Repository)
+
+
+### Running
+
+Set up GCP Cloud Run **Job** using appropriate resources (X Gb).
+
+Run using:
+
+gcloud run jobs execute gcp-era5 \
+    --region=us-central1 \
+    --args='MHD,1978'
+
---
+++ ml_baselines/gcp_era5/get_gcp_era5.py
@@ -0,0 +1,219 @@
+import xarray as xr
+import numpy as np
+import gcsfs
+import tempfile
+import argparse
+
+
+site_coords_dict = {
+        "MHD":[53.3267, -9.9046], 
+        "RPB":[13.1651, -59.4321], 
+        "CGO":[-40.6833, 144.6894], 
+        "GSN":[33.2924, 126.1616],
+        "JFJ":[46.547767, 7.985883], 
+        "CMN":[44.1932, 10.7014], 
+        "THD":[41.0541, -124.151], 
+        "ZEP":[78.9072, 11.8867],
+        "SMO": [-14.2474, -170.5644]
+    }
+
+lats_grid = np.array([0, 5, 5, 0, -5, -5, -5, 0, 5, 10, 10, 0, -10, -10, -10, 0, 10])
+lons_grid = np.array([0, 0, 5, 5, 5, 0, -5, -5, -5, 0, 10, 10, 10, 0, -10, -10, -10])
+
+arco_era5_location = 'gs://gcp-public-data-arco-era5/ar/full_37-1h-0p25deg-chunk-1.zarr-v3'
+
+levels = [850, 500]
+
+variables_3d = ["u_component_of_wind", "v_component_of_wind"]
+variables_2d = ["10m_u_component_of_wind", "10m_v_component_of_wind",
+                "surface_pressure", "boundary_layer_height"]
+
+
+def longitude_to_360(lons):
+    """
+    Convert longitudes to 0-360 range.
+    """
+    return np.where(lons < 0, lons + 360, lons)
+
+
+def retrieve_grid(arco_era5_location):
+    """ Retrieve the grid from the ARCO ERA5 dataset.
+
+    Args:
+        arco_era5_location (str): The location of the ARCO ERA5 dataset.
+    Returns:
+        tuple: A tuple containing the grid latitudes, longitudes, and levels.
+    """
+
+    with xr.open_zarr(
+        arco_era5_location,
+        chunks=None,
+        storage_options=dict(token='anon'),
+        ) as ds:
+        grid_lats = ds["latitude"].values
+        grid_lons = ds["longitude"].values
+        grid_levels = ds["level"].values
+
+    return grid_lats, grid_lons, grid_levels
+
+
+def define_points(site):
+    """ Define points based on the site coordinates and the grid.
+
+    Args:
+        site (str): The site for which to define points.
+    Returns:
+        tuple: A tuple containing the latitude points, longitude points, and their indices.
+    """
+
+    points_lat = []
+    points_lon = []
+    
+    site_lat, site_lon = site_coords_dict[site]
+    
+    # creating a grid system with +/- 5 and 10 degrees latitude and longitude from the site of interest
+    points_lat.append(lats_grid + site_lat)
+    points_lon.append(lons_grid + site_lon)
+
+    points_lon = np.concatenate(points_lon)
+    points_lat = np.concatenate(points_lat)
+    points = np.arange(len(points_lat))
+
+    return points_lat, longitude_to_360(points_lon), points
+
+
+def find_closest_grid_points(grid_lats, grid_lons,
+                            points_lat, points_lon, points):
+    """
+    Find the closest grid points for the given latitude, longitude, and level.
+
+    Args:
+        grid_lats (np.ndarray): The grid latitude values.
+        grid_lons (np.ndarray): The grid longitude values.
+        points_lat (np.ndarray): The latitude points to match.
+        points_lon (np.ndarray): The longitude points to match.
+        points (np.ndarray): The point indices.
+
+    Returns:
+        tuple: A tuple containing the closest latitude points, longitude points, and level points.
+    """
+    global levels
+
+    # Find values in grid_lats and grid_lons that are closest to the points
+    def find_closest_indices(grid, points):
+        indices = []
+        for point in points:
+            # Calculate the absolute difference between the grid and the point
+            diff = np.abs(grid - point)
+            # Find the index of the minimum difference
+            index = np.argmin(diff)
+            indices.append(index)
+        return indices
+
+    closest_lat_indices = find_closest_indices(grid_lats, points_lat)
+    closest_lon_indices = find_closest_indices(grid_lons, points_lon)
+
+    points_lat_closest = grid_lats[closest_lat_indices]
+    points_lon_closest = grid_lons[closest_lon_indices]
+
+    lats = xr.DataArray(points_lat_closest, dims=["points"], coords={"points": points})
+    lons = xr.DataArray(points_lon_closest, dims=["points"], coords={"points": points})
+    levels = xr.DataArray(levels, dims=["levels"], coords={"levels": levels})
+
+    return lats, lons, levels
+
+
+def get(year, lats, lons, levels):
+    """ Retrieve the dataset for the specified latitudes, longitudes, and levels.
+    Args:
+        year (int): The year of interest.
+        lats (xarray.DataArray): The latitude points.
+        lons (xarray.DataArray): The longitude points.
+        levels (xarray.DataArray): The level points.
+    Returns:
+        xarray.Dataset: The dataset containing the specified variables.
+    """
+
+    time_slice = slice(f"{year}-01-01T00:00:00",
+                       f"{year+1}-01-01T00:00:00")
+
+    variables_3d = ["u_component_of_wind", "v_component_of_wind"]
+    variables_2d = ["10m_u_component_of_wind", "10m_v_component_of_wind",
+                    "surface_pressure", "boundary_layer_height"]
+    
+    with xr.open_zarr(
+            arco_era5_location,
+            chunks=None,
+            storage_options=dict(token='anon'),
+            ) as ds:
+
+        ds_points_3d = ds[variables_3d].sel(
+            latitude=lats,
+            longitude=lons,
+            level=levels,
+            time=time_slice).load()
+
+        ds_points_2d = ds[variables_2d].sel(
+            latitude=lats,
+            longitude=lons,
+            time=time_slice).load()
+
+    ds_points = xr.merge([ds_points_3d, ds_points_2d])
+
+    return ds_points
+
+
+def save_to_bucket(ds_points, bucket_name, file_name):
+    """ Save the dataset to a Google Cloud Storage bucket.
+
+    Args:
+        ds_points (xarray.Dataset): The dataset to save.
+        bucket_name (str): The name of the GCP bucket.
+        file_name (str): The name of the file to save.
+    """
+
+    with tempfile.NamedTemporaryFile(suffix=".nc") as tmp:
+        ds_points.to_netcdf(tmp.name, mode="w", engine="netcdf4", format="NETCDF4")
+        fs = gcsfs.GCSFileSystem(token='google_default')
+        fs.put(tmp.name, f"{bucket_name}/{file_name}")
+
+    # Uncomment to save locally
+    # ds_points.to_netcdf(f"/app/output/{file_name}", mode='w', format='NETCDF4')
+
+
+def run(site, year):
+    """ Run the process to retrieve and save ERA5 data for a specific site.
+
+    Args:
+        site (str): The site for which to retrieve data.
+        year (int): The year of interest.
+    """
+    global levels
+
+    if site not in site_coords_dict:
+        raise ValueError(f"Site {site} not found in site coordinates dictionary.")
+
+    grid_lats, grid_lons, grid_levels = retrieve_grid(arco_era5_location)
+
+    for level in levels:
+        if level not in grid_levels:
+            raise ValueError(f"Level {level} not found in grid levels.")
+
+    points_lat, points_lon, points = define_points(site)
+
+    lats, lons, levels = find_closest_grid_points(grid_lats, grid_lons,
+                            points_lat, points_lon, points)
+
+    ds_points = get(year, lats, lons, levels)
+
+    save_to_bucket(ds_points, "ml-baselines-era5", f"era5-{site}-{year}.nc")
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Retrieve ERA5 data for a specific site.")
+    parser.add_argument("site", type=lambda s: s.strip(), help="Site for which to retrieve data (e.g., 'MHD').")
+    parser.add_argument("year", type=lambda s: int(s.strip()), help="Year of interest (e.g., 1978).")
+    args = parser.parse_args()
+
+    run(args.site, args.year)
